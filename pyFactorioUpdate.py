0f2049989336f495bbc41c78772fba678c9f7328
GitGerby <gearnsc@gmail.com>
2019-03-08 09:10:48 -0500

    move download to function

--- pyFactorioUpdate.py
+++ pyFactorioUpdate.py
@@ -5,6 +5,14 @@ import os as os
 import argparse as pr
 import tarfile as tf
 
+def download_file(url,dest_dir,dest_file):
+  r = rq.get(url, stream=True)
+  with open(dest_dir + dest_file, 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=128):
+      fd.write(chunk)
+  fd.close()
+  print "downloaded {url} to {dest}".format(url,dest_dir + dest_file)
+
 parser = pr.ArgumentParser()
 parser.add_argument('-e', '--experimental', help="Use Factorio's experimental track rather than stable", action='store_true')
 args = parser.parse_args()
@@ -14,7 +22,7 @@ current_archive_ts = os.path.getctime(current_archive)
 current_archive_datetime = dt.datetime.fromtimestamp(current_archive_ts)
 
 tmp_dir = '/tmp/factorio-updater'
-tmp_file = '/tmp/factorio-updater/archive.tmp'
+tmp_file = 'archive.tmp'
 tmp_staging = '/tmp/factorio-updater/staging'
 
 if not os.path.exists(tmp_dir):
@@ -43,10 +51,7 @@ server_datetime = dt.datetime.strptime(server_datestring, '%a, %d %b %Y %H:%M:%S
 
 if server_datetime > current_archive_datetime:
   print('new version of Factorio detected, beginning download')
-  r = rq.get(url,stream=True)
-  with open(tmp_file, 'wb') as fd:
-    for chunk in r.iter_content(chunk_size=128):
-      fd.write(chunk)
+  download_file(url, tmp_dir, tmp_file)
   print ('downloaded new version to {}'.format(tmp_file))
 else:
   print('Factorio is already up to date')
