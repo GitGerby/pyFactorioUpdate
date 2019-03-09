600ddb1e6db8e5369025e41c515b636b99e65589
GitGerby <gearnsc@gmail.com>
2019-03-09 18:48:12 -0500

    add travis

--- pyFactorioUpdate.py
+++ pyFactorioUpdate.py
@@ -4,7 +4,7 @@ pyFactorioUpdate was created to eliminate the toil involved in updating a headle
 When run the script will compare the last-modified header of the Factorio archive available for
 download against the creation time of the most recently downloaded archive, if the web version
 is newer it will be fetched and installed. Using -e or --experimental will compare against the
-experimental version of Factorio rather than the stable version. To force a download and 
+experimental version of Factorio rather than the stable version. To force a download and
 installation regardless of timestamps use -f or --force.
 '''
 
