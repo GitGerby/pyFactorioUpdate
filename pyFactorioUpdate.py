cf12c2ccc28a20c96c2f2727f2e4f8f26e89ed51
GitGerby <gearnsc@gmail.com>
2019-03-13 20:44:28 -0400

    start logging changes

--- pyFactorioUpdate.py
+++ pyFactorioUpdate.py
@@ -14,6 +14,7 @@ import os
 import shutil
 import tarfile
 import subprocess
+import logging
 import requests
 
 
@@ -69,14 +70,21 @@ PARSER.add_argument(
      'Exits with 0 if no new package availble, 10 if newer version available.'
      ),
     action='store_true')
+PARSER.add_argument(
+    '--log_level',
+    choices=['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'],
+    default='INFO')
 ARGS = PARSER.parse_args()
+
+LOGGER = logging.getLogger('factorio_updater')
 
 CURRENT_ARCHIVE = '/opt/factorio-updater/current'
 if os.path.exists(CURRENT_ARCHIVE):
     CURRENT_ARCHIVE_TS = os.path.getctime(CURRENT_ARCHIVE)
     CURRENT_ARCHIVE_DATETIME = datetime.utcfromtimestamp(CURRENT_ARCHIVE_TS)
 else:
-    print('Unable to determine timestamp of currently installed instance')
+    logging.warning(
+        'Unable to determine timestamp of currently installed instance')
     CURRENT_ARCHIVE_DATETIME = datetime.fromtimestamp(0)
 
 TMP_DIR = ARGS.tmp_dir
@@ -94,28 +102,28 @@ if os.path.exists(TMP_FILE):
 SERVER_DATETIME, URL = get_latest_version(ARGS.experimental)
 
 if SERVER_DATETIME > CURRENT_ARCHIVE_DATETIME or ARGS.force:
-    print('New version of Factorio available.')
+    LOGGER.info('New version of Factorio available.')
 
     if ARGS.check_only:
         exit(10)
 
     download_file(URL, TMP_FILE)
-    print('Downloaded new version to {}.'.format(TMP_FILE))
+    LOGGER.debug('Downloaded new version to %s.', TMP_FILE)
 
     if not os.path.exists(TMP_STAGING):
-        print('Creating staging folder {}.'.format(TMP_STAGING))
+        LOGGER.debug('Creating staging folder %s.', TMP_STAGING)
         os.mkdir(TMP_STAGING, 0o755)
 
     NEW_FACTORIO = extract_factorio(TMP_FILE, TMP_STAGING)
 
-    print('Stopping Factorio.')
+    LOGGER.debug('Stopping Factorio.')
     return_code = subprocess.run(['systemctl', 'stop', 'factorio']).returncode
     if return_code != 0:
         raise RuntimeError
-    print('Stopped Factorio.')
+    LOGGER.debug('Stopped Factorio.')
 
     print('Copying new files.')
-    # TODO: Figure out where the current version is installed. This assumes /opt/.
+    # TODO(GitGerby): Figure out where the current version is installed. This assumes /opt/.
     return_code = subprocess.run(['cp', '-R', NEW_FACTORIO,
                                   '/opt/']).returncode
     if return_code != 0:
@@ -131,7 +139,7 @@ if SERVER_DATETIME > CURRENT_ARCHIVE_DATETIME or ARGS.force:
     print('Updating current archive.')
     shutil.move(TMP_FILE, CURRENT_ARCHIVE)
 else:
-    print('Factorio is already up to date.')
+    LOGGER.info('Factorio is already up to date.')
 
 if os.path.exists(TMP_STAGING):
     shutil.rmtree(TMP_STAGING)
