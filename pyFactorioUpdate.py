94cefbfce97b55fa05f3187da71b3e8df061184a
GitGerby <gearnsc@gmail.com>
2019-07-27 15:48:32 -0400

    add mod functionality

--- pyFactorioUpdate.py
+++ pyFactorioUpdate.py
@@ -16,6 +16,45 @@ import tarfile
 import subprocess
 import logging
 import requests
+import yaml
+
+
+def check_mods():
+    '''Check whether mods need updating and compile a list of updatable mods'''
+    update_needed = False
+
+    manifest = requests.get(MODMANIFEST, allow_redirects=True)
+    config = dict(yaml.safe_load(manifest.content))
+    mods_to_update = []
+
+    for mod in config['mods']:
+        mod_path = os.path.join('/opt/factorio/mods/', mod['name'] + '.zip')
+        if os.path.exists(mod_path):
+            local_mod_time = datetime.utcfromtimestamp(
+                os.path.getctime(mod_path))
+        else:
+            local_mod_time = datetime.fromtimestamp(0)
+        metadata = requests.get(
+            'https://mods.factorio.com/api/mods/{name}'.format(
+                name=mod['name']))
+        released = datetime.strptime(metadata['releases'][-1]['released_at'],
+                                     '%a, %d %b %Y %H:%M:%S %Z')
+        if released > local_mod_time:
+            update_needed = True
+            mod_url = 'https://mods.factorio.com/' + metadata['releases'][
+                'download_url'] + '?username=' + APIUSER + '&token=' + APITOKEN
+            mods_to_update.append({
+                'name': mod['name'],
+                'path': mod_path,
+                'url': mod_url
+            })
+    return mods_to_update, update_needed
+
+
+def update_mods(updateable_mods):
+    '''Downloads mods that need updating'''
+    for mod in updateable_mods:
+        download_file(mod['mod_url'], mod['path'])
 
 
 def download_file(src, dest):
@@ -35,15 +74,13 @@ def extract_factorio(archive, dest):
     return os.path.join(dest, 'factorio')
 
 
-def get_latest_version(experimental):
+def get_latest_version(url):
     '''Returns the datetime of the package available for download and the URL
        to retrieve that package.
     '''
-    url = 'https://www.factorio.com/get-download/{revision}/headless/linux64'.format(
-        revision="latest" if experimental else "stable")
     response = requests.head(url, allow_redirects=True)
-    return (datetime.strptime(response.headers['Last-Modified'],
-                              '%a, %d %b %Y %H:%M:%S %Z'), url)
+    return datetime.strptime(response.headers['Last-Modified'],
+                             '%a, %d %b %Y %H:%M:%S %Z')
 
 
 PARSER = argparse.ArgumentParser()
@@ -79,6 +116,7 @@ PARSER.add_argument(
     help='User mod api auth',
 )
 PARSER.add_argument('--api_token', help='Token for mod api auth')
+PARSER.add_argument('--mod_manifest', help='Mod manifest location.')
 
 # TODO: Allow selection of logging level at run time.
 # PARSER.add_argument(
@@ -113,8 +151,12 @@ else:
 CHECKMODS = ARGS.mods
 APIUSER = ARGS.api_user
 APITOKEN = ARGS.api_token
+MODMANIFEST = ARGS.mod_manifest
 if CHECKMODS and (APIUSER == '' or APITOKEN == ''):
-    print('Mod check requested but no credentials supplied')
+    LOGGER.warning('Mod check requested but no credentials supplied')
+    CHECKMODS = False
+if CHECKMODS and MODMANIFEST == '':
+    LOGGER.warning('Mod check requested but no manifest specified')
     CHECKMODS = False
 
 TMP_DIR = ARGS.tmp_dir
@@ -128,37 +170,54 @@ if not os.path.exists(TMP_DIR):
 if os.path.exists(TMP_FILE):
     LOGGER.info('Cleaning up old temp file.')
     os.remove(TMP_FILE)
+URL = 'https://www.factorio.com/get-download/{revision}/headless/linux64'.format(
+    revision='latest' if ARGS.experimental else 'stable')
+SERVER_DATETIME = get_latest_version(URL)
 
-SERVER_DATETIME, URL = get_latest_version(ARGS.experimental)
+server_update = False
 
-if SERVER_DATETIME > CURRENT_ARCHIVE_DATETIME or ARGS.force:
-    LOGGER.info('New version of Factorio available.')
+if SERVER_DATETIME > CURRENT_ARCHIVE:
+    LOGGER.info('Server update available')
+    server_update = True
+
+if CHECKMODS:
+    LOGGER.info('Checking for mod updates')
+    mods, mod_update = check_mods()
+    if mod_update:
+        LOGGER.info('Mod updates available')
+
+if server_update or mod_update or ARGS.force:
 
     if ARGS.check_only:
         exit(10)
-
-    download_file(URL, TMP_FILE)
-    LOGGER.debug('Downloaded new version to %s.', TMP_FILE)
-
-    if not os.path.exists(TMP_STAGING):
-        LOGGER.debug('Creating staging folder %s.', TMP_STAGING)
-        os.mkdir(TMP_STAGING, 0o755)
-
-    NEW_FACTORIO = extract_factorio(TMP_FILE, TMP_STAGING)
 
     LOGGER.debug('Stopping Factorio.')
     return_code = subprocess.run(['systemctl', 'stop', 'factorio']).returncode
     if return_code != 0:
         raise RuntimeError
+
     LOGGER.debug('Stopped Factorio.')
 
-    LOGGER.debug('Copying new files.')
-    # TODO(GitGerby): Figure out where the current version is installed. This assumes /opt/.
-    return_code = subprocess.run(['cp', '-R', NEW_FACTORIO,
-                                  '/opt/']).returncode
-    if return_code != 0:
-        raise RuntimeError
-    LOGGER.debug('Copied new files.')
+    if server_update:
+        download_file(URL, TMP_FILE)
+        LOGGER.debug('Downloaded new version to %s.', TMP_FILE)
+
+        if not os.path.exists(TMP_STAGING):
+            LOGGER.debug('Creating staging folder %s.', TMP_STAGING)
+            os.mkdir(TMP_STAGING, 0o755)
+
+        NEW_FACTORIO = extract_factorio(TMP_FILE, TMP_STAGING)
+
+        LOGGER.debug('Copying new files.')
+        # TODO(GitGerby): Figure out where the current version is installed. This assumes /opt/.
+        return_code = subprocess.run(['cp', '-R', NEW_FACTORIO,
+                                      '/opt/']).returncode
+        if return_code != 0:
+            raise RuntimeError
+        LOGGER.debug('Copied new files.')
+
+    if mod_update:
+        update_mods(mods)
 
     LOGGER.debug('Starting Factorio.')
     return_code = subprocess.run(['systemctl', 'start', 'factorio']).returncode
